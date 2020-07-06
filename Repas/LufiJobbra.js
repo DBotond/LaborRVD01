f63b28caf18ade42f4d1572535bd7c3c26678967
Répás Kristóf <gyasztunder@MacBook-Pro.local>
2020-07-06 11:06:06 +0200

    Add existing file

---
+++ Repas/LufiJobbra.js
@@ -0,0 +1,18 @@
+function LufiJobbra(x, y, radius) {
+    this.body = Bodies.circle(x, y, radius);
+    this.radius = radius;
+    World.add(world, this.body);
+
+    this.show = function() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        strokeWeight(5);
+        stroke(255);
+        fill(88, 106, 247);
+        circle(0, 0, this.radius * 2);
+        pop();
+    }
+}
